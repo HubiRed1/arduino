6a91b06b30f1b504f60f8cbd05d83bf9fc8c9120
HubiRed1 <hubert.redlinski88@gmail.com>
2026-03-18 19:14:37 +0100

    git commit

--- drs+rpm/sketch.ino
+++ drs+rpm/sketch.ino
@@ -1,60 +1,50 @@
-// F1 Junior Engineer Program - Cadillac F1 Simulation
-// System: Shift Lights & DRS Logic
+const int POT_PIN = A0;
+const int DRS_BUTTON = 2;
+const int LED_GREEN = 8;
+const int LED_YELLOW = 9;
+const int LED_BLUE = 10;
+const int LED_DRS = 11;
 
-const int POT_PIN = A0;      // Potencjometr symuluje obroty silnika (RPM)
-const int DRS_BUTTON = 2;    // Przycisk aktywacji DRS
-const int LED_GREEN = 8;     // Dioda: Niskie obroty
-const int LED_YELLOW = 9;    // Dioda: Średnie obroty
-const int LED_BLUE = 10;     // Dioda: SHIFT (Zmień bieg!)
-const int LED_DRS = 11;      // Kontrolka aktywnego DRS
+bool drsState = false;
+bool lastButtonState = HIGH;
 
 void setup() {
-  pinMode(DRS_BUTTON, INPUT_PULLUP); // Przycisk z rezystorem podciągającym
+  pinMode(DRS_BUTTON, INPUT_PULLUP);
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_YELLOW, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_DRS, OUTPUT);
   
-  Serial.begin(9600); // Konsola do podglądu danych (Telemetria)
+  Serial.begin(9600);
 }
 
 void loop() {
-  // 1. ODCZYT DANYCH (Symulacja telemetrii)
   int rawValue = analogRead(POT_PIN);
-  int rpm = map(rawValue, 0, 1023, 0, 15000); // Mapowanie na 0-15k RPM
-  bool drsRequested = (digitalRead(DRS_BUTTON) == LOW); // Przycisk wciśnięty
+  int rpm = map(rawValue, 0, 1023, 0, 15000);
+  
+  bool currentButtonState = digitalRead(DRS_BUTTON);
 
-  // 2. LOGIKA SHIFT LIGHTS
+  if (currentButtonState == LOW && lastButtonState == HIGH) {
+    drsState = !drsState;
+    delay(50);
+  }
+  lastButtonState = currentButtonState;
+
+  digitalWrite(LED_DRS, drsState);
+
   digitalWrite(LED_GREEN, rpm > 10000);
   digitalWrite(LED_YELLOW, rpm > 12500);
   
-  // Miganie niebieską diodą przy 14k RPM (Over-rev protection)
   if (rpm > 14000) {
-    digitalWrite(LED_BLUE, (millis() / 50) % 2); // Szybkie miganie bez delay()!
+    digitalWrite(LED_BLUE, (millis() / 50) % 2);
   } else {
     digitalWrite(LED_BLUE, LOW);
   }
 
-// 3. LOGIKA DRS
-  // System zadziała TYLKO jeśli:
-  // - Przycisk jest wciśnięty (LOW)
-  // - ORAZ obroty są powyżej 8000
-  if (digitalRead(DRS_BUTTON) == LOW) {
-    digitalWrite(LED_DRS, HIGH);
-  } else {
-    digitalWrite(LED_DRS, LOW);
-  }
-
-// 4. WYWYSŁANIE DANYCH DO INŻYNIERÓW (Serial Monitor)
   Serial.print("RPM: ");
   Serial.print(rpm);
   Serial.print(" | DRS Status: ");
-  
-  if (digitalRead(LED_DRS) == HIGH) {
-    Serial.println("ACTIVE");
-  } else {
-    Serial.println("OFF");
-  }
+  Serial.println(drsState ? "ACTIVE" : "OFF");
 
-  delay(10); // Mały odstęp dla stabilności symulacji
+  delay(10);
 }
